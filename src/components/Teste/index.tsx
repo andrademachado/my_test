b00f33c86779d5ef1d893d7352ee7cea364f0836
Lígia Andrade <andradeligiamp@gmail.com>
2024-10-22 09:03:08 -0300

    Testes em react/typescript de reinderização de imagens com o uso  links diretos do google drive .

---
+++ src/components/Teste/index.tsx
@@ -0,0 +1,13 @@
+const Teste = ()=>(
+
+    <div>
+        <h1>Minha Imagem do Google Drive</h1>
+        <img src="https://drive.google.com/uc?export=view&id=1TXh8t4TsWigIQk8S5q70AGPCsdYlh1KC" alt="Descrição da imagem" />
+    </div>
+
+
+)
+
+
+
+export default Teste;
